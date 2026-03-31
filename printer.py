3d1de6927be16eab5bad29f05e4af6868953c45c
OutsiderXI <154287825+outsiderXI@users.noreply.github.com>
2026-03-30 22:27:09 -0400

    Update printer.py

--- printer.py
+++ printer.py
@@ -1,10 +1,14 @@
+from pathlib import Path
 from escpos.printer import Usb
-from config import *
+from config import IMAGE_DIR
 
-def print_image(path):
+def print_image(card_id):
 
-    p = Usb(PRINTER_VENDOR_ID, PRINTER_PRODUCT_ID)
+    path = IMAGE_DIR / f"{card_id}.jpg"
 
+    if not path.exists():
+        print(f"Image not found: {path}")
+        return
+
+    p = Usb(0x04b8,0x0202)
     p.image(str(path))
-    p.cut()
-    p.close()
