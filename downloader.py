91438f94ec26ed2a4f9e6d0ee559247f33d0b28c
OutsiderXI <154287825+outsiderXI@users.noreply.github.com>
2026-04-01 23:01:49 -0400

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -288,8 +288,8 @@ def _process_and_save_image_bytes(content, path):
 
     img = Image.open(BytesIO(content))
     scale = PRINTER_MAX_WIDTH / img.width
-    w = max(1, int(img.width * scale * 1.2))
-    h = max(1, int(img.height * scale * 1.2))
+    w = max(1, int(img.width * scale))
+    h = max(1, int(img.height * scale))
 
     img = img.resize((w, h), Image.LANCZOS)
     img = img.convert("L").convert("1", dither=Image.FLOYDSTEINBERG)
